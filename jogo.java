02457471b8a45844301f245abc3f0f0ddc58993f
valterMoura <valtermoura20@gmail.com>
2020-10-22 20:26:45 -0300

    Primeiro Commit

--- jogo.java
+++ jogo.java
@@ -3,8 +3,7 @@ package src;
 public class jogo {
     public static void main(String[] args) {
        
-        System.out.println( "Hello GitHub!" );
-        System.out.println( "Acrecentei uma linha!" );
+    	 //Vamos lá pessoal esse é o nosso arquivo!!!
 	    
     }
 }
