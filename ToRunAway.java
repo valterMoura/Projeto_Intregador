16db0800964b6e17cba0b19330e7076d594928ce
katiaschvabe <katiaschvabe@hotmail.com>
2020-11-10 16:52:41 -0300

    Update ToRunAway.java
    Aprimorado história inicial. Ainda há detalhes.

--- ToRunAway.java
+++ ToRunAway.java
@@ -10,53 +10,57 @@ public class ToRunAway {
 		// Menu
         do {
 		System.out.println("TO RUN AWAY \n");
-		System.out.println(" Op√ß√µes \n");
+		System.out.println("VocÍ esta preparado(a) para provar seu potencial e suas habilidades em um jogo totalmente diferenciado?");
+		System.out.println("Apresentamos TO RUN AWAY! O jogo que faltava para vocÍ testar seus conhecimentos como programador.");
+		System.out.println("VocÍ topa esse desafio?");
+		System.out.println(" OpÁıes \n");
 		System.out.println("1 - Jogar");
 		System.out.println("2 - Creditos");
 		System.out.println("3 - Sair \n");
-		System.out.print("Digite a op√ß√£o: ");
+		System.out.print("Digite a opÁ„o: ");
 		opcao = entrada.nextInt();
 		switch (opcao) {
 		case 1:
-			System.out.println("\nDigiter o seu nome: ");
+			System.out.println("\nDigite o seu nome: ");
 			nome = entrada.next();
-			System.out.print("\nDigiter o seu sexo: ");
+			System.out.print("\nDigite o seu sexo: ");
 			System.out.println("M => Masculino  F => Feminino");
 			sexo = entrada.next();
 			if(sexo.equalsIgnoreCase("M")) {
-				System.out.println("\nA historia\n");
-				System.out.println("Um jovem programador, chamado "+nome+" trabalha a pouca tempo na empresa chamada TI Universal,"
-						+ " empresa que estava sofrendo um grande roubo de informa√ß√µes sigilosas \npor hackers,\n"+nome+" por ser novato foi utilizado como laranja pelos hackers"
-								+ " que tamb√©m trabalhavam na mesma empresa, foram implatadas muitas provas em seu sistema no qual trabalhava, \nsem que o mesmo soubesse.\n"+
-						"A empresa contratou uma equipe de hackers para descobrir quem estava roubando essas informa√ß√µes da empresa e divulgando-as,\nao descobrir as provas implantadas no nome do "+
-								nome+", o presidente da impresa aciona seu seguran√ßas para que localize e capture o jovem programador,\nnesse mesmo exato momento aciona o sistema de seguran√ßa do predio para que "+
-						"ele n√£o fuja, esse sistema de seguran√ßa,\nbloqueia os elevadores que s√≥ podem descer de um em um andar, portas e janela s√£o trancadas automaticamente,\n"+
-								"para o "+nome+" fugir, ter√° que hackear o sistema de seguran√ßa e fugir de todos os seguran√ßas da empresa,\nque se localizam em pontos estrategios para que "+nome+" n√£o sai sem ser visto.");
+				System.out.println("\n A historia\n");
+				System.out.println("Um jovem programador, chamado " + nome + " trabalha h· pouco tempo na empresa TI Universal,"
+						+ " empresa que estava sofrendo roubo de informaÁıes sigilosas \n por hackers,\n" + nome + " por ser novato, foi utilizado como laranja pelos hackers."
+								+ " Os hackers, que tambÈm trabalhavam na mesma empresa, implantaram muitas provas no usu·rio do sistema no qual " + nome + " trabalhava, \n sem que o mesmo soubesse.\n"+
+						"A TI Universal contratou uma equipe de hackers para descobrir quem estava roubando essas informaÁıes da empresa e divulgando-as,\n ao descobrir as provas implantadas no nome de "+
+								nome+", o presidente da empresa aciona a seguranÁa para que localize e capture o jovem programador. \n Nesse exato momento o sistema de seguranÁa do predio È acionado para que "+
+						"ele n„o fuja. O sistema de seguranÁa,\n bloqueia os elevadores que sÛ podem descer um andar, portas e janela s„o trancadas automaticamente,\n"+
+								"e para o "+nome+" fugir, ter· que hackear o sistema de seguranÁa e fugir de todos os seguranÁas da empresa,\n que est„o localizados em pontos estratÈgios para que ninguÈm saia sem ser visto.");
 			}else if(sexo.equalsIgnoreCase("F")) {
 				System.out.println("\nA historia\n");
-				System.out.println("Uma jovem programadora, chamado "+nome+" trabalha a pouca tempo na empresa chamada TI Universal,"
-						+ " empresa que estava sofrendo um grande roubo de informa√ß√µes sigilosas \npor hackers,\n"+nome+" por ser novata foi utilizada como laranja pelos hackers"
-								+ " que tamb√©m trabalhavam na mesma empresa, foram implatadas muitas provas em seu sistema no qual trabalhava, \nsem que a mesma soubesse.\n"+
-						"A empresa contratou uma equipe de hackers para descobrir quem estava roubando essas informa√ß√µes da empresa e divulgando-as,\nao descobrir as provas implantadas no nome da "+
-								nome+", o presidente da impresa aciona seu seguran√ßas para que localize e capture a jovem programadora,\nnesse mesmo exato momento aciona o sistema de seguran√ßa do predio para que "+
-						"ela n√£o fuja, esse sistema de seguran√ßa,\nbloqueia os elevadores que s√≥ podem descer de um em um andar, portas e janela s√£o trancadas automaticamente,\n"+
-								"para a "+nome+" fugir, ter√° que hackear o sistema de seguran√ßa e fugir de todos os seguran√ßas da empresa,\nque se localizam em pontos estrategios para que "+nome+" n√£o sai sem ser visto.");
+				System.out.println("Uma jovem programadora, chamada " + nome + " trabalha h· pouco tempo na empresa TI Universal,"
+						+ " empresa que estava sofrendo roubo de informaÁıes sigilosas \n por hackers,\n" + nome + " por ser novata, foi utilizada como laranja pelos hackers."
+						+ " Os hackers, que tambÈm trabalhavam na mesma empresa, implantaram muitas provas no usu·rio do sistema no qual " + nome + " trabalhava, \n sem que a mesma soubesse.\n"+
+				"A TI Universal contratou uma equipe de hackers para descobrir quem estava roubando essas informaÁıes da empresa e divulgando-as,\n ao descobrir as provas implantadas no nome de "+
+						nome+", o presidente da empresa aciona a seguranÁa para que localize e capture a jovem programadora. \n Nesse exato momento o sistema de seguranÁa do predio È acionado para que "+
+				"ela n„o fuja. O sistema de seguranÁa,\n bloqueia os elevadores que sÛ podem descer um andar, portas e janela s„o trancadas automaticamente,\n"+
+						"e para o "+nome+" fugir, ter· que hackear o sistema de seguranÁa e fugir de todos os seguranÁas da empresa,\n que est„o localizados em pontos estratÈgios para que ninguÈm saia sem ser visto.");
 			}
 			break;
 			
 		case 2:
 			System.out.println(" CREDITOS ");
-			System.out.println("Desenvolvedor: VALTER DE MOURA PEREIRA");
+			
 			System.out.println("Desenvolvedor: DAIMON DA SILVA");
-			System.out.println("Desenvolvedor: K√ÅTIA SCHVABE");
-			System.out.println("Desenvolvedor: RENATO BERTONI LARDOSA SANTOS");
+			System.out.println("Desenvolvedor: KATIA SCHVABE");
+			System.out.println("Desenvolvedor: VALTER DE MOURA PEREIRA");
 			System.out.println("Desenvolvedor: VICTOR RODRIGUES ROCHA");
-			System.out.println("\n Op√ß√£o");
+			System.out.println("Desenvolvedor: RENATO BERTONI LARDOSA SANTOS");
+			System.out.println("\n OpÁ„o");
 			System.out.print("2 - Voltar ");
 			opcao = entrada.nextInt();			
 			break;
 		case 3:
-			System.out.println("\nVoc√™ saiu do jogo");
+			System.out.println("\n VocÍ saiu do jogo");
 			break;
 		}
         }while(opcao == 2);
