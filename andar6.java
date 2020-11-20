b86cafe62972515472db2bb1e79a7e6ae29a9517
daimonssg <72233576+daimonssg@users.noreply.github.com>
2020-11-20 10:47:33 -0300

    Create andar6.java
    No andar6 o jogador tera que escolher a opção correta para acessar o 5 andar caso escolha a opção invalida, o jogador ficara sem oxigênio.

---
+++ andar6.java
@@ -0,0 +1,27 @@
+
+public class andar6  {
+
+	public static void main(String[] args) {
+		System.out.println("Departamento de Recursos Humanos");
+	    System.out.println("Estavamos aguardando você, sua presença aqui NÃO É BEM VINDA, por isso se prepare");
+	    System.out.println ("Neste andar, nosso sistema de segurança, esta integrado ao sistema de combate a incêndio"
+	    + "Para acessar o 5 andar tera que Digite os comandos de acesso\n"
+	    + "O sistema de combate a incêndio é dotado de Bombas de Vácuo que em caso de acionamento todo Oxigênio do andar sera Sequestrado\n"
+	    + "Durante o primeiro minuto do Sequestro do Oxigênio será iniciado uma contagem de 60 segundos, para se digitar o código correto\n"
+	    + "Se o código digitado estiver correto será disponibilizado, uma mascara conectada a um cilindro de oxigênio com autonomia de 120 segundos de ar\n"
+	    + "se durante os 120 segundos o próximo código, não for digitado de forma correta o Ar do cilindro ira acabar;");
+	    System.out.print("Game Over");
+	    System.out.println("Escolha uma função que some a idade do jogador + o número do andar; o código sendo escolhido de forma correta o jogador acessa o 5 andar\n"
+	    + "caso erre sera dado inicio a contagem de 60 segundos\n");
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+
+	}
+
+}
